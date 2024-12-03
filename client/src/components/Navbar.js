8f9d6a6949ca0987688474168a908075e03909e5
revalinocalvin <revalinocalvin@gmail.com>
2024-12-03 09:03:46 +0700

    navbar backbone added

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,4 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import '../styles/navbar.css'; // Import the CSS file for styling
 
-export default App;
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src="../hog-logo-bg.png" alt="Logo" />
+      </div>
+      <div className="navbar-links">
+        <a href="#dashboard">Dashboard</a>
+        <a href="#rewards">Rewards</a>
+      </div>
+      <div className="navbar-icons">
+        <img src="/path/to/bell-icon.png" alt="Notifications" className="icon" />
+        <img src="/path/to/user-profile.png" alt="User" className="icon" />
+        <button className="user-button">User</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
