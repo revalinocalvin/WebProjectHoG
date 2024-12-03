89461aeaff65419ddf7589ca2a4ca6adcdc39e77
revalinocalvin <revalinocalvin@gmail.com>
2024-12-03 08:15:03 +0700

    Update routes.js

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MainPage from './pages/Public/MainPage';
+import FAQPage from './pages/Public/FAQPage';
+import BookingTutorialPage from './pages/Public/BookingTutorialPage';
+import LoginPage from './pages/Auth/LoginPage';
+import RegistrationPage from './pages/Auth/RegistrationPage';
+import DashboardPage from './pages/User/DashboardPage';
+import PointsRedeemPage from './pages/User/PointsRedeemPage';
+import ProfilePage from './pages/User/ProfilePage';
+import EditProfilePage from './pages/User/EditProfilePage';
+import HistoryPage from './pages/User/HistoryPage';
+import AnnouncementPage from './pages/User/AnnouncementPage';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const AppRoutes = () => (
+  <Router>
+    <Routes>
+      {/* Public Routes */}
+      <Route path="/" element={<MainPage />} />
+      <Route path="/faq" element={<FAQPage />} />
+      <Route path="/booking-tutorial" element={<BookingTutorialPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegistrationPage />} />
+      
+      {/* Protected Routes */}
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/points-redeem" element={<PointsRedeemPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/edit-profile" element={<EditProfilePage />} />
+      <Route path="/history" element={<HistoryPage />} />
+      <Route path="/announcement" element={<AnnouncementPage />} />
+    </Routes>
+  </Router>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default AppRoutes;
